623c6fa7d5eca1b26fc8096b2bb9c111aeac49fe
MustafaHilmiYAVUZHAN <85347708+MustafaHilmiYAVUZHAN@users.noreply.github.com>
2025-03-07 00:51:20 +0300

    Update __init__.py

--- CTkTable/__init__.py
+++ CTkTable/__init__.py
@@ -3,12 +3,15 @@ CustomTkinter Table widget
 Author: Akash Bora
 License: MIT
 This is a custom table widget for customtkinter.
+
+TableGenerator is faster
 Homepage: https://github.com/Akascape/CTkTable
 """
 
 """
 Another Homepage: https://github.com/MustafaHilmiYAVUZHAN
 """
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
-from .ctktable import CTkTable
+from .MiniTable import CTkTableMini
+from .TableGenerator import TableGenerator
